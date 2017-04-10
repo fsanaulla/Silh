feb3d8494481d82a3432fc3aa9796ca7bbb265a5
faiaz <fayaz.sanaulla@gmail.com>
2017-04-10 15:47:38 +0300

    logs added

--- app/controllers/ClientController.scala
+++ app/controllers/ClientController.scala
@@ -5,7 +5,6 @@ import com.mohiva.play.silhouette.api.Silhouette
 import forms.ValidationForms._
 import play.api.i18n.MessagesApi
 import play.api.libs.json.Json
-import play.api.mvc.Action
 import services.ClientService
 import utils.Implicits._
 import utils.MyEnv
@@ -15,11 +14,15 @@ import scala.concurrent.ExecutionContext.Implicits.global
 /**
   * Created by faiaz on 12.03.17.
   */
-class ClientController @Inject()(clientService: ClientService,
-                                 val silhouette: Silhouette[MyEnv],
-                                 val messagesApi: MessagesApi) extends AuthController {
+class ClientController @Inject()(
+                                  clientService: ClientService,
+                                  val silhouette: Silhouette[MyEnv],
+                                  val messagesApi: MessagesApi
+                                ) extends AuthController {
 
   def addClient = SecuredAction.async { implicit req =>
+    log.info("Save new client")
+
     val client = addClientForm.bindFromRequest.get
     clientService.insertClient(client.first_name, client.last_name)
       .map { c =>
@@ -28,7 +31,9 @@ class ClientController @Inject()(clientService: ClientService,
       }
   }
 
-  def getClient(userId: Long) = Action.async { implicit req =>
+  def getClient(userId: Long) = SecuredAction.async { implicit req =>
+    log.info(s"Get client by id: $userId")
+
     clientService.getClient(userId).map {
       case Some(u) =>
         log.info(s"Successfully retrieved client $u for id:$userId")
@@ -40,6 +45,8 @@ class ClientController @Inject()(clientService: ClientService,
   }
 
   def updateClient(userId: Long) = SecuredAction.async { implicit req =>
+    log.info(s"Update client with id: $userId")
+
     val update = updateClientForm.bindFromRequest.get
     clientService.updateClient(userId, update.first_name, update.last_name)
       .map { res =>
@@ -49,6 +56,8 @@ class ClientController @Inject()(clientService: ClientService,
   }
 
   def deleteClient(userId: Long) = SecuredAction.async { implicit req =>
+    log.info(s"Delete client by id: $userId")
+
     clientService.deleteClient(userId)
       .map { _ =>
         log.info(s"Successfully deleting for id:$userId")
@@ -57,6 +66,7 @@ class ClientController @Inject()(clientService: ClientService,
   }
 
   def getAllClient = SecuredAction.async { implicit req =>
+    log.info("Get all clients")
     clientService.allClients
       .map { seq =>
         log.info("Returning all clients")
--- app/controllers/LogsController.scala
+++ app/controllers/LogsController.scala
@@ -22,16 +22,21 @@ class LogsController @Inject()(
                               ) extends AuthController {
 
   def addLogNote = SecuredAction.async { implicit req =>
-    val log = addLogForm.bindFromRequest.get
-    logService.addLog(log.userId, log.content).map(l => Ok(Json.toJson(l)))
+    log.info("Save new Log Note")
+
+    val logNote = addLogForm.bindFromRequest.get
+    logService.addLog(logNote.userId, logNote.content).map(l => Ok(Json.toJson(l)))
   }
 
   def updateLogNote(userId: Long) = SecuredAction.async { implicit req =>
+    log.info(s"Update Log Note with id: $userId")
     val content = singleLogsUpdate.bindFromRequest.get
     logService.updateLogs(userId, content).map(res => Ok(s"Update status: $res"))
   }
 
   def getLogNote(userId: Long) = SecuredAction.async { implicit req =>
+    log.info(s"Get Log Note by id: $userId")
+
     logService.getLogs(userId).map {
       case Some(l) => Ok(Json.toJson(l))
       case _ => BadRequest(s"LogNote with Client ID: $userId not exist")
@@ -39,10 +44,14 @@ class LogsController @Inject()(
   }
 
   def deleteLogNote(userId: Long) = SecuredAction.async { implicit req =>
+    log.info(s"Delete Log Note by id: $userId")
+
     logService.deleteLogs(userId).map(_ => NoContent)
   }
 
   def getAllLogsNote = SecuredAction.async { implicit req =>
+    log.info("Get all Log Note")
+
     logService.getAllLogs.map(seq => Ok(Json.obj("logs" -> seq)))
   }
 }
